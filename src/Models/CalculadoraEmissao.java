e5c0ab06bca05a90b1cdc13fa2db0bbc798b9164
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2024-11-19 16:54:23 -0300

    finalização menu

--- src/Models/CalculadoraEmissao.java
+++ src/Models/CalculadoraEmissao.java
@@ -7,20 +7,15 @@ public class CalculadoraEmissao {
     }
 
     public static void calcularEmissaoCarbono(Usuario usuario) {
-        // Calcular emissões relacionadas ao transporte
         double emissaoTransporte = usuario.getMeioTransportePrincipal().calcularEmissaoTransporte();
 
-        // Calcular emissões relacionadas à energia
         double emissaoEnergia = usuario.getMoradiaPrincipal().calcularEmissaoEnergia();
 
-        // Calcular emissões relacionadas aos alimentos
         double emissaoAlimentos = usuario.getMoradiaPrincipal().calcularEmissaoAlimentos();
 
-        // Total de emissões
         double totalEmissao = emissaoTransporte + emissaoEnergia + emissaoAlimentos;
 
-        // Exibir os resultados
-        System.out.println("Emissões de CO2 geradas:");
+        System.out.println("\nEmissões de CO2 geradas:");
         System.out.println("Transporte: " + emissaoTransporte + " kgCO2");
         System.out.println("Energia: " + emissaoEnergia + " kgCO2");
         System.out.println("Alimentos: " + emissaoAlimentos + " kg");
